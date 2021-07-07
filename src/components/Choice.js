f75c8b9fabd5746e48a3a66b46cbe70bfc3ac41c
anique.qureshi <anique.qureshi@gmail.com>
2021-07-07 13:59:29 -0400

    Updated Yup Validation

--- src/components/Choice.js
+++ src/components/Choice.js
@@ -1,13 +1,18 @@
-import { Formik, FieldArray, Form, Field } from 'formik';
-import * as Yup from 'yup';
+import { Formik, FieldArray, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
 
 const initialValues = {
     choiceGroup: '',
     choiceItems: ['']
 }
 
-const validationSchema = Yup.object({
-
+const validationSchema = yup.object().shape({
+    choiceGroup: yup.string().required('You must enter a Group value'),
+    choiceItems: yup.array()
+    .of(yup.string().trim().required("Please enter a value for each Choice"))
+    .min(2,'At least two choices must be entered')
+    
+    //,
 })
 
 const onSubmit = values => {
@@ -28,6 +33,8 @@ const Choice = () => {
                 <Form>
                     <label>Group</label>
                     <Field type='text' id='choiceGroup' name='choiceGroup' />
+                    <ErrorMessage name='choiceGroup' />
+                    
                     <FieldArray id ='choiceItems' name='choiceItems'>
                         {fieldArrayProps => {
                             const {push, remove, form} = fieldArrayProps
@@ -37,6 +44,7 @@ const Choice = () => {
                             return <div>
                                 {choiceItems.map((choiceItem, index) => (
                                     <div key={index}>
+                                        <label>Choice {index + 1}</label>
                                         <Field name={`choiceItems[${index}]`} />
                                         {
                                             index >1 &&
@@ -49,6 +57,7 @@ const Choice = () => {
                             </div>
                         }}
                     </FieldArray>
+                    <ErrorMessage name='choiceItems' />
                     <button type='submit'>Save</button>
                 </Form>
             </Formik>
